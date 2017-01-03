08fa0b9472254320d4ab126bce8bb459c9eb8898
Irina <irbris@mail.ru>
2017-01-03 14:08:17 +0200

    Add files via upload

---
+++ 04. Работа с переменными.script.js
@@ -0,0 +1,17 @@
+/* Работа с переменными
+важность: 2
+
+    Объявите две переменные: admin и name.
+    Запишите в name строку "Василий".
+    Скопируйте значение из name в admin.
+    Выведите admin (должно вывести «Василий»).
+http://learn.javascript.ru/task/hello-variables
+*/
+
+var admin;
+var name;
+
+name = "Василий";
+admin = name;
+
+alert(admin);
